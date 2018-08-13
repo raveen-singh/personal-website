85d942940340b006a2bf4356ee75aa0a3b71c588
raveen-singh <emailraveensingh@gmail.com>
2018-08-12 21:11:36 -0400

    reorganized

--- assets/js/blotter.js
+++ assets/js/blotter.js
@@ -13,64 +13,59 @@ function distanceBetweenPoints(x1, y1, x2, y2) {
   return Math.sqrt((a * a) + (b * b));
 }
 
-$(document).ready(function() {
+$(document)
+  .ready(function () {
 
-  var styleProperties = {
+    var styleProperties = {
       family: "Oswald",
       size: 100,
       fill: "#171717",
-      paddingLeft : 100,
-     paddingRight:100,
-    paddingTop: 100,
-    paddingBottom: 100
-  };
+      paddingLeft: 100,
+      paddingRight: 100,
+      paddingTop: 100,
+      paddingBottom: 100
+    };
 
-  var text = new Blotter.Text("Raveen", styleProperties);
+    var text = new Blotter.Text("Raveen", styleProperties);
 
-  var material = new Blotter.ChannelSplitMaterial();
+    var material = new Blotter.ChannelSplitMaterial();
 
-  var blotter = new Blotter(material, {
-      texts: text
-  });
+    var blotter = new Blotter(material, {texts: text});
 
-  var myScope = blotter.forText(text);
+    var myScope = blotter.forText(text);
 
-
-blotter.on("ready", function() {
+    blotter.on("ready", function () {
       myScope.appendTo(document.getElementById("blob"));
-                scope2.appendTo(document.getElementById("blob"));
+      scope2.appendTo(document.getElementById("blob"));
+
+    });
+
+    $('html').mousemove(function (event) {
+      var currentX = event.pageX;
+      var currentY = event.pageY;
+
+      var offset = $('#blob').offset();
+      var width = $('#blob').width();
+      var height = $('#blob').height();
+
+      var centerX = offset.left + width / 2;
+      var centerY = offset.top + height / 2;
+
+      var angle = 0;
+      angle = angleBetweenPointsInDegrees(centerX, centerY, currentX, currentY);
+      var blur = 0;
+      blur = distanceBetweenPoints(centerX, centerY, currentX, currentY) * 0.0001;
+      // blur = 0.1; var blur = 0.1;
+
+      var dis = distanceBetweenPoints(centerX, centerY, currentX, currentY);
+
+      $('#angle').html(angle);
+      $('#blob-pos').html('offset: ' + offset.left + ' x: ' + centerX + ',y: ' + centerY + '');
+      $('#dis').html(dis);
+      $('#mp').html(currentX + ',' + currentY);
+
+      myScope.material.uniforms.uRotation.value = angle;
+      myScope.material.uniforms.uOffset.value = blur;
+    });
 
   });
-
-
-$('html').mousemove(function(event){
-  var currentX = event.pageX;
-  var currentY = event.pageY;
-
- var offset = $('#blob').offset();
- var width = $('#blob').width();
- var height = $('#blob').height();
-
- var centerX = offset.left + width / 2;
- var centerY = offset.top + height / 2;
-
-
-  var angle = 0;
-  angle = angleBetweenPointsInDegrees(centerX, centerY, currentX, currentY);
-  var blur = 0;
-  blur = distanceBetweenPoints(centerX, centerY, currentX, currentY)* 0.0001;
-  // blur = 0.1;
-  // var blur = 0.1;
-  
-   var dis = distanceBetweenPoints(centerX, centerY, currentX, currentY);
-  
-   $('#angle').html(angle);
-   $('#blob-pos').html( 'offset: '+ offset.left  +   ' x: ' + centerX+ ','+ 'y: ' + centerY + '');
-   $('#dis').html(dis);
-   $('#mp').html(currentX+ ','+ currentY);
-
-   myScope.material.uniforms.uRotation.value = angle;
-   myScope.material.uniforms.uOffset.value = blur;
-});
-
-});
---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+$(document)
+    .ready(function () {
+        $('#fullpage').fullpage({
+            //options here
+            autoScrolling: true,
+            scrollHorizontally: true
+        });
+
+        //methods
+        $
+            .fn
+            .fullpage
+            .setAllowScrolling(false);
+    });
